52838bd124261a553373cfb4fab75d60c44d3f4f
senganull <ayanokoji.roito@gmail.com>
2024-12-22 05:48:03 +0900

    commit

--- src/main/java/com/github/roitoayanokoji/tutorial/Tutorial.java
+++ src/main/java/com/github/roitoayanokoji/tutorial/Tutorial.java
@@ -28,12 +28,20 @@ public final class Tutorial extends JavaPlugin {
                 // equalsIgnoreCase を使うことで、大文字と小文字を区別せずに比較します。
                 // つまり、「/hello」や「/Hello」、「/HELLO」など、どの形式でも正しく処理されます。
                 if (sender instanceof Player){
+                    //senderはコマンドを実行した人(プレイヤーやコンソール)
+                    //instanceof Playerは「このsenderがPlayerであるか？」を確認する条件
                     Player player = (Player) sender;
+                    //ここはまだよくわかっていない
                     player.sendMessage("こんにちは" + player.getName() + "さん");
+                    //player.sendMessageはプレイヤーに()の中のメッセージを送るメソッド
+                    //player.getName()はコマンドを実行したプレイヤーの名前を取得するメソッド
                 }else{
                     sender.sendMessage("このコマンドはプレイヤーから実行する必要があります。");
+                    //ここはifのとこの条件式が(sender instanceof Player)なのでこのコマンドを実行したのがプレイヤーかどうかで
+                    //elseはifがfalseつまりプレイヤーでない場合であったときに実行される部分
                 }
                 return true;
+                //trueを返すことで、プラグインはコマンドを正しく処理したことをMinecraftサーバーに通知する。
                 //コマンドが実行された場合は、trueを返して当メソッドを抜ける。
             }
             return false;
